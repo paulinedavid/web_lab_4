f521b33e804c2b8e521362701c1e7a34c42b013c
Hugo PANEL <hugopanel@gmail.com>
2023-09-25 16:00:07 +0200

    Ex 7: create Basebutton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <button :role="props.role" :style="props.style" :disabled="props.disabled">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup>
+const props = defineProps(['role', 'style', 'disabled']);
+</script>
+
+
+<style scoped>
+button {
+    background-color: green;
+    color: white;
+    border: solid 1px white;
+    border-radius: 5px;
+    box-shadow: rgba(0, 0, 0, .3) 0 0 5px;
+    padding: 5px;
+    transition: all ease-out 100ms;
+}
+
+button:hover {
+    box-shadow: rgba(0, 0, 0, .5) 0 3px 10px;
+}
+
+button:active {
+    box-shadow: none;
+}
+
+button:disabled {
+    background-color: rgb(151, 201, 151);
+    color: white;
+    box-shadow: none;
+    border-color: rgb(230, 230, 230);
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
         Here comes the content of the HomePage.
+        <BaseButton> 
+            Active button
+        </BaseButton>
+        <BaseButton disabled> 
+            Disabled button
+        </BaseButton>
     </div>
 </template>
+
+<script setup>
+import BaseButton from '../components/BaseButton.vue'
+</script>
 
 <style scoped>
 div {
