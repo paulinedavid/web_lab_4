33ac7c212572456133943ca25a53f7fae7bafe2a
Hugo PANEL <hugopanel@gmail.com>
2023-09-25 16:25:50 +0200

    Ex 8: color palette and prop or BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,18 +1,32 @@
 <template>
-    <button :role="props.role" :style="props.style" :disabled="props.disabled">
+<button :role="props.role" :style="props.style" :disabled="props.disabled" :class="color">
         <slot></slot>
     </button>
 </template>
 
 <script setup>
-const props = defineProps(['role', 'style', 'disabled']);
+const props = defineProps(['role', 'style', 'disabled', 'color']);
+var colors = ['primary', 'warn', 'danger'];
+
+var color;
+
+const init = async () => {
+    if (colors.includes(props.color)) {
+        color = props.color;
+    } else {
+        color = colors[0];
+    }
+};
+
+init();
+</script>
+
+<script>
 </script>
 
 
 <style scoped>
 button {
-    background-color: green;
-    color: white;
     border: solid 1px white;
     border-radius: 5px;
     box-shadow: rgba(0, 0, 0, .3) 0 0 5px;
@@ -29,9 +43,37 @@ button:active {
 }
 
 button:disabled {
-    background-color: rgb(151, 201, 151);
-    color: white;
     box-shadow: none;
     border-color: rgb(230, 230, 230);
+}
+
+button.primary {
+    background-color: green;
+    color: white;
+}
+
+button:disabled.primary {
+    background-color: rgb(151, 201, 151);
+    color: white;
+}
+
+button.warn {
+    background-color: rgb(242, 233, 64);
+    color: black;
+}
+
+button:disabled.warn {
+    background-color: rgb(255, 252, 199);
+    color: rgb(148, 148, 148);
+}
+
+button.danger {
+    background-color: rgb(225, 36, 36);
+    color: white;
+}
+
+button:disabled.danger {
+    background-color: rgb(255, 177, 177);
+    color: white;
 }
 </style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,49 @@
 <template>
     <div>
         Here comes the content of the HomePage.
-        <BaseButton> 
-            Active button
-        </BaseButton>
-        <BaseButton disabled> 
-            Disabled button
-        </BaseButton>
+        Buttons:
+        <ul style="list-style: none;">
+            <li>
+                <BaseButton> 
+                    Active default button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton disabled> 
+                    Disabled default button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="primary"> 
+                    Active primary button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="primary" disabled> 
+                    Disabled primary button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="warn"> 
+                    Active warn button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="warn" disabled> 
+                    Disabled warn button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="danger"> 
+                    Active danger button
+                </BaseButton>
+            </li>
+            <li>
+                <BaseButton color="danger" disabled> 
+                    Disabled danger button
+                </BaseButton>
+            </li>
+        </ul>
     </div>
 </template>
 
