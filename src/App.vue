f47a15f1eda3f8b8b98213135bf9516b46b36065
Hugo PANEL <hugopanel@gmail.com>
2023-09-25 14:29:26 +0200

    Ex 3: remove vue CLI placeholders

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,3 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Once placeholders are deleted, the project is almost empty... In this example, App.vue does not depend on any other component.
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
